65533b64a35cb6123a55ffce567b4dfcb796ea66
Ivan Garcerant <igarcerant@gmail.com>
2022-11-19 10:32:39 -0500

    factorizacion de codigo comun en rutina reutilizable

--- main.c
+++ main.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int prompt(char*mensaje)
+{
+    int a;
+    printf("%s ", mensaje);
+    fflush(stdout);
+    scanf("%d", &a);
+    return a;
+}
+
 int main (int argc,char ** argv)
 {
     int a, b, c;
     printf("hello, world!\n");
-    printf("dime el primer numero: ");
-    fflush(stdout);
-    scanf("%d",&a);
-    printf("dime el segundo numero: ");
-    fflush(stdout);
-    scanf("%d",&b);
+    a = prompt("dime el primer numero:");
+    b = prompt("dime el segundo numero:");
     c = a + b;
     printf("la suma de %d mas %d es igual a %d\n", a, b, c);
     return EXIT_SUCCESS;
